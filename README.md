cbe10d3107978cad1dd2ca33188116d8ddb0faa5
zarm95 <59035854+zarm95@users.noreply.github.com>
2023-02-25 18:06:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-18ikks
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-18ikks)
---
+++ src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+const Contact = (props) => {
+  const name = props.name;
+  console.log(name);
+  const email = props.email;
+  console.log(email);
+  const phoneNumber = props.phoneNumber;
+  console.log(phoneNumber);
+
+  return (
+    <div className="friend-item">
+      <h3>{name}</h3>
+      <h4>
+        📧{email}
+        <br />
+        📞{phoneNumber}
+      </h4>
+      <button>Delete</button>
+    </div>
+  );
+};
+
+export const App = (
+  // write your HTML code here
+
+  <div>
+    <h1 className="text-center">My contacts list 📱</h1>
+
+    <div>
+      <Contact
+        name="emanul"
+        email="emanual@gmail.com"
+        phoneNumber="045656525"
+      />
+      <Contact
+        name="john"
+        email="john@gmail.com"
+        phoneNumber="045656525"
+      />
+      <Contact
+        name="emanul"
+        email="emanual@gmail.com"
+        phoneNumber="045656525"
+      />
+      <Contact
+        name="emanul"
+        email="emanual@gmail.com"
+        phoneNumber="045656525"
+      />
+      <Contact
+        name="emanul"
+        email="emanual@gmail.com"
+        phoneNumber="045656525"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(App, document.getElementById('root'));
